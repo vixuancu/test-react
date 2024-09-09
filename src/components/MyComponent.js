632403a9e118fe0723f74b8b1ef9103358c0596a
vixuancu <vixuancu2004@gmail.com>
2024-09-09 09:39:43 +0700

    video 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,9 +8,23 @@ class MyComponent extends React.Component {
     address: "hoidanit",
     age: 26,
   };
+  handleClick(event) {
+    // console.log(">> click me mu button");
+    console.log(event.target);
+  }
+  handleMouseOver(event) {
+    // console.log(">> click me mu button");
+    console.log(event);
+  }
   //JSX
   render() {
-    return <div>my first component My name is {this.state.name}</div>;
+    return (
+      <div>
+        my first component My name is {this.state.name}
+        <button onClick={this.handleClick}>click me</button>
+        <button onMouseOver={this.handleMouseOver}>Hover me</button>
+      </div>
+    ); // dùng cú pháp this
   }
 }
 export default MyComponent;
