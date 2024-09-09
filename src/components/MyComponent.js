561c89fd378b00bb8df3c81cb8b914e6844dff4a
vixuancu <vixuancu2004@gmail.com>
2024-09-09 10:27:21 +0700

    video 21

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -11,10 +11,9 @@ class MyComponent extends React.Component {
   handleClick = (event) => {
     // console.log(">> click me mu button");
     // console.log(event.target);
-    console.log("age", this.state.age);
-
+    // console.log("age", this.state.age);
     this.setState({
-      name: "vixuancu",
+      name: event.target.value,
       age: Math.floor(Math.random() * 100 + 1),
     });
   };
@@ -22,19 +21,30 @@ class MyComponent extends React.Component {
     // console.log(">> click me mu button");
     console.log(event);
   }
+  handleOnChangeInput = (event) => {
+    // console.log(event);
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  handleOnSubmit = (event) => {
+    event.preventDefault(); // không tải lại trang
+    console.log(this.state);
+  };
   //JSX
   render() {
     return (
       <div>
-        my first component My name is {this.state.name}
-        <button
-          onClick={(event) => {
-            this.handleClick(event);
-          }}
-        >
-          click me
-        </button>
-        <button onMouseOver={this.handleMouseOver}>Hover me</button>
+        my first component My name is {this.state.name} and i'm {this.state.age}
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            type="text"
+            onChange={(event) => {
+              this.handleOnChangeInput(event);
+            }}
+          />
+          <button>Submit</button>
+        </form>
       </div>
     ); // dùng cú pháp this
   }
