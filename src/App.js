c54afe09139cb4f55924d163439f5f1e7666068e
vixuancu <vixuancu2004@gmail.com>
2024-09-09 09:14:29 +0700

    video 17

--- src/App.js
+++ src/App.js
@@ -3,26 +3,35 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import MyComponent from "./components/MyComponent";
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
+import React from "react";
 
-  return (
-    <div>
-      hello world với xuancu &amp; hoidanit
-      <MyComponent></MyComponent>
-    </div>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        hello world với xuancu &amp; hoidanit
+        <MyComponent></MyComponent>
+      </div>
+    );
+  }
+}
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>vixuancuđjsadkjb</p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
-};
+// const App = () => {
+//   const count = useSelector((state) => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>vixuancuđjsadkjb</p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// };
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,14 +3,14 @@
 
 import React from "react";
 class MyComponent extends React.Component {
+  state = {
+    name: "reic",
+    address: "hoidanit",
+    age: 26,
+  };
   //JSX
   render() {
-    return (
-      <div>
-        my first component
-        {Math.random()}
-      </div>
-    );
+    return <div>my first component My name is {this.state.name}</div>;
   }
 }
 export default MyComponent;
