cc95ebe6f44112ba58369b200177cca07e4f7d5e
vixuancu <vixuancu2004@gmail.com>
2024-09-11 23:33:07 +0700

    46. Setup Bootstrap 5 & React Router Dom v6

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-import React from "react";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        hello world với xuancu &amp; hoidanit
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
-}
-
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>vixuancuđjsadkjb</p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// };
+const App = () => {
+  return <div className="app-container">hello world</div>;
+};
 
 export default App;
