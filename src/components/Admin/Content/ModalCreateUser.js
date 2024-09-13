861d2d411237116a8305658731fafe0a923518d3
vixuancu <vixuancu2004@gmail.com>
2024-09-13 17:40:13 +0700

    60. API Services - Customize Axios

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -2,8 +2,9 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FaPlusCircle } from "react-icons/fa";
-import axios from "axios";
+
 import { toast } from "react-toastify";
+import { postCreateNewUser } from "../../../services/apiService";
 const ModalCreateUser = (props) => {
   const { show, setShow } = props;
   // const [show, setShow] = useState(false);
@@ -61,25 +62,16 @@ const ModalCreateUser = (props) => {
     //   userImage: image,
     // };
     // console.log(data);
-    const data = new FormData();
-    data.append("email", email);
-    data.append("password", password);
-    data.append("username", username);
-    data.append("role", role);
-    data.append("userImage", image);
 
-    let res = await axios.post(
-      "http://localhost:8081/api/v1/participant",
-      data
-    );
-    console.log("check data", res.data);
-    if (res.data && res.data.EC === 0) {
-      toast.success(res.data.EM);
+    let data = await postCreateNewUser(email, password, username, role, image);
+    console.log("check data", data);
+    if (data && data.EC === 0) {
+      toast.success(data.EM);
       handleClose();
     }
 
-    if (res.data && res.data.EC !== 0) {
-      toast.error(res.data.EM);
+    if (data && data.EC !== 0) {
+      toast.error(data.EM);
     }
   };
   return (
---
+++ src/services/apiService.js
@@ -0,0 +1,13 @@
+// import axios from "axios";
+import axios from "../utils/axiosCustomize";
+const postCreateNewUser = (email, password, username, role, image) => {
+  //submit
+  const data = new FormData();
+  data.append("email", email);
+  data.append("password", password);
+  data.append("username", username);
+  data.append("role", role);
+  data.append("userImage", image);
+  return axios.post("api/v1/participant", data);
+};
+export { postCreateNewUser };
