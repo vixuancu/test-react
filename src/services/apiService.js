417caf06e5565805ab6a7a7bf5e8474d0c9a2ed6
vixuancu <vixuancu2004@gmail.com>
2024-09-16 21:31:04 +0700

    81. Design Danh Sách Bài Thi Của User - Display List Quiz

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -35,6 +35,10 @@ const postLogin = (email, password) => {
 const postRegister = (email, password, username) => {
   return axios.post(`api/v1/register`, { email, password, username });
 };
+
+const getQuizByUser = () => {
+  return axios.get("api/v1/quiz-by-participant");
+};
 export {
   postCreateNewUser,
   getAllUser,
@@ -43,4 +47,5 @@ export {
   getUserWithPaginate,
   postLogin,
   postRegister,
+  getQuizByUser,
 };
