0c693b524517d2b0f1e0cef71ddc13dd497f2ba1
vixuancu <vixuancu2004@gmail.com>
2024-09-17 23:27:15 +0700

    83. Process Data - Xử Lý Data Phía Frontend

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
+import _ from "lodash";
 const DetailQuiz = (props) => {
   const params = useParams();
   const quizId = params.id;
@@ -10,6 +11,30 @@ const DetailQuiz = (props) => {
   const fetchQuesTions = async () => {
     let res = await getDataQuiz(quizId);
     console.log("check questions", res);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answers = [];
+          let questionDesc,
+            image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDesc = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+          console.log("value", value, "key", key);
+
+          return { questionId: key, answers, questionDesc, image };
+        })
+        .value();
+      console.log("check raw", data);
+    }
   };
   //   console.log("check param", params);
   return <div className="detail-quiz-container">chi tiết quiz</div>;
